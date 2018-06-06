38a078775156b6379ae2c77d02c321dca99dc042
daveonebillion <djwilson394@gmail.com>
2018-06-06 12:47:17 +0100

    documentation not up to date

--- lc_extractor.py
+++ lc_extractor.py
@@ -39,19 +39,16 @@ Outputs:
 def region_mask(x, y, slope, intercept, height):
 	mask = (y > slope*x+intercept-height/2.) & (y < slope*x+intercept+height/2.)
 	return mask 
-	
-def ensure_dir(d):
-	if not os.path.exists(d):
-		os.makedirs(d)
-	
+
 def filewriter(time, counts, error, save_path, filename): 
 	# writes lightcurves to dat files
-	ensure_dir(save_path)
+	if not os.path.exists(save_path):
+		os.makedirs(save_path)
 	fl=open((save_path+filename),'w')
 	for t, c, e in zip(time, counts, error):
 	  fl.write('%f %f %f\n'%(t, c, e))
 
-def lc_maker(star='unknown', file_path=os.getcwd()+'/', 
+def old_lc_maker(star='unknown', file_path=os.getcwd()+'/', 
              save_path=os.getcwd()+'/lightcurves/', bin_time=1., 
              qual_check=True, plot=True):
 
@@ -168,12 +165,168 @@ def lc_maker(star='unknown', file_path=os.getcwd()+'/',
 	if plot == True:
 		plot_lc(star, all_time, all_counts, all_error, bin_time)
 
-def plot_lc(star, time, counts, error, bin_time):
-	plt.figure(star+'_'+str(bin_time)+'s')
+def extract_counts(header, data, bins, qual_check, seg):
+    slope = header['SP_SLP_'+seg]
+    sp_intercept = header['SP_LOC_'+seg]
+    sp_height = float(header['SP_HGT_'+seg])
+
+    #background regions
+    bk1_intercept = header['B_BKG1_'+seg]
+    bk1_height = float(header['B_HGT1_'+seg])
+    bk2_intercept = header['B_BKG1_'+seg]
+    bk2_height = float(header['B_HGT1_'+seg])
+
+    #data 
+    x = data['XCORR']
+    y = data['YCORR']
+    time = data['TIME']
+    w = data['WAVELENGTH']
+    dq = data['DQ']
+
+    #mask out flagged pixels
+    if qual_check == True:
+        x, y, time, w = x[dq==0], y[dq==0], time[dq==0], w[dq==0]
+
+    #mask out airglow from lyman alpha and oi
+    wave_mask = (w < 1214.)|(w > 1217.)&(w < 1301.)|(w > 1307.)
+    x, y, time = x[wave_mask], y[wave_mask], time[wave_mask]
+
+    #extract lightcurve from spectrum
+    sp_mask = region_mask(x, y, slope, sp_intercept, sp_height)
+    sp_lc = np.histogram(time[sp_mask], bins)
+    t,sp_counts = sp_lc[1][:-1], sp_lc[0]
+
+    #background
+    bk1_mask = region_mask(x, y, slope, bk1_intercept, bk1_height)
+    bk1_lc = np.histogram(time[bk1_mask], bins)
+    bk2_mask = region_mask(x, y, slope, bk2_intercept, bk2_height)
+    bk2_lc = np.histogram(time[bk2_mask], bins)
+    bk_counts = (bk1_lc[0]+bk2_lc[0])*(sp_height/(bk1_height+bk2_height)) #sum background counts and normalise to spectrum area
+
+    #background subtraction 
+    counts_bksub = sp_counts - bk_counts 
+
+    return t, counts_bksub
+
+
+def lc_maker(star='unknown', file_path=os.getcwd()+'/', save_path=os.getcwd()+'/lightcurves/', bin_time=1., qual_check=True, plot=True):
+
+    #find the corrtag files, and end the script if there aren't any
+    tag_files = glob.glob(file_path+'*corrtag*')
+    if len(tag_files) == 0:
+        print ('There are no corrtag files in file_path :(.')
+        os._exit(1)
+
+    #find all rootnames
+    rootnames = np.array([], dtype=str)
+    bands = np.array([], dtype=str)
+    exp_times = np.array([], dtype=float)
+    for tag in tag_files:
+        with fits.open(tag) as hdul:
+            rootname = hdul[0].header['ROOTNAME']
+            if rootname not in rootnames:
+                rootnames = np.append(rootnames,rootname)
+                bands = np.append(bands, hdul[0].header['DETECTOR'])
+                exp_times = np.append(exp_times, hdul[1].header['EXPTIME'])
+    #print(rootnames, len(rootnames))
+    #print(bands, len(bands))
+                
+       # rootnames= np.append(rootnames, fits.open(tag)[0].header['ROOTNAME'])
+#    rootnames = np.unique(rootnames)
+ 
+    
+    
+    #make arrays to store combined lightcurve in 
+    #removing this to make it easier to work with more than one star at a time
+    #all_time = np.array([], dtype=float)
+    #all_counts = np.array([], dtype=float)
+    #all_error = np.array([], dtype =float)
+
+    for rootname, band, exp_time in zip(rootnames, bands, exp_times):
+        nbins = int(exp_time/bin_time)
+        counts = np.zeros(nbins)
+        
+        if band == 'FUV':
+
+            #checks if both segments are available 
+            segs = ['a', 'b']
+            if (file_path+rootname+'_corrtag_a.fits') not in tag_files:
+                segs = ['b']
+            if (file_path+rootname+'_corrtag_b.fits') not in tag_files:
+                segs = ['a']
+
+            for seg in segs:
+                tag_file = rootname+'_corrtag_'+seg+'.fits'
+
+                seg = seg.upper() #header keywords are uppercase
+
+                hdul = fits.open(file_path+tag_file)
+                header = hdul[1].header
+                data = hdul[1].data
+
+                star = hdul[0].header['TARGNAME']
+               
+                t, counts_bksub = extract_counts(header, data, nbins, qual_check, seg)
+                
+                counts += counts_bksub
+            
+        
+        elif band == 'NUV':
+
+            tag_file = rootname+'_corrtag.fits'
+            segs = ['A', 'B', 'C']
+            hdul = fits.open(file_path+tag_file)
+            header = hdul[1].header
+            data = hdul[1].data
+            star = hdul[0].header['TARGNAME']
+            
+            for seg in segs:
+               
+                t, counts_bksub = extract_counts(header, data, nbins, qual_check, seg)
+                
+                counts += counts_bksub
+
+
+        #calculate photon noise
+        error = counts**0.5 
+
+        #convert time to absolute time
+        t_adg = t + (header['EXPSTART']*86400.)
+
+        #convert to counts s-1 
+        counts_sec = counts/bin_time
+        error_sec = error/bin_time
+
+
+
+        filewriter(t_adg, counts_sec, error_sec, save_path, rootname+'_'+str(bin_time)+'s_lc.dat')
+        if plot == True:
+            plot_lc(star, rootname, t_adg, counts_sec, error_sec, bin_time)
+        
+        hdul.close()
+    
+    
+        
+
+       # all_time = np.concatenate((all_time, t_adg), axis =0)	
+        #all_counts = np.concatenate((all_counts, counts_sec))
+        #all_error = np.concatenate((all_error, error_sec))
+
+    #filewriter(all_time, all_counts, all_error, save_path, star+'_'+str(bin_time)+'s_lc_combined.dat') 
+
+ #   if plot == True:
+  #      plot_lc(star, all_time, all_counts, all_error, bin_time)
+
+        
+        
+        
+def plot_lc(star, rootname, time, counts, error, bin_time):
+	plt.figure(star+'_'+rootname+'_'+str(bin_time)+'s')
 	plt.subplots_adjust(top=0.99, right =0.99)
 	plt.errorbar(time-time[0], counts, yerr = error, ls='none', marker='o')
 	plt.xlabel('Time (s)', size=20)
 	plt.ylabel('Counts (s$^{-1}$)', size=20)
 	plt.show()
 	
-
+#test
+lc_maker(file_path = '0845_lc_test/', save_path = '0845_lc_test/test/', bin_time=5.)
